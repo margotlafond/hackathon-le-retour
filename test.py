25059dd5af80cedb3be63204b168693c05b65af1
mae-leguevel <leguevelmaelig@gmail.com>
2025-01-31 16:19:24 +0100

    ca bouge

--- test.py
+++ test.py
@@ -4,63 +4,68 @@ TITLE = "Taquin"
 HEIGHT = 350
 WIDTH = 250
 
-sequence = [[1, 4, 6, 0, 5, 9, 8, 3, 2], 
-            [0, 4, 6, 1, 5, 9, 8, 3, 2], 
-            [4, 0, 6, 1, 5, 9, 8, 3, 2], 
-            [4, 5, 6, 1, 0, 9, 8, 3, 2]]
+sequence = [
+    [1, 4, 6, 0, 5, 9, 8, 3, 2], 
+    [0, 4, 6, 1, 5, 9, 8, 3, 2], 
+    [4, 0, 6, 1, 5, 9, 8, 3, 2], 
+    [4, 5, 6, 1, 0, 9, 8, 3, 2]
+]
 
 class Tile(ft.TextField):
     def __init__(self, number: int, line: int, column: int) -> None:
-        super().__init__(value=str(number), read_only=True, text_align=ft.TextAlign.CENTER, width=100, height=100)
-        self._number = number
+        super().__init__(
+            value=str(number),
+            read_only=True,
+            text_align=ft.TextAlign.CENTER,
+            width=60, height=60
+        )
+        self.number = number
         self.line = line  
         self.column = column  
-       
-        
-
-    @property
-    def number(self) -> int:
-        return self._number
-
-    @number.setter
-    def number(self, new_number: int) -> None:
-        self._number = new_number
-        self.value = str(new_number)  # Mise à jour de l'affichage
 
 
 class Board:
     def __init__(self, page):
         self.page = page
-        self.squares = []  
+        self.squares = []
+        self.current_step = 0  # Indice actuel de la séquence
 
     def create_tiles(self, position):
+        """Crée les tuiles à partir d'une liste plate (1D)."""
         self.squares = [
             Tile(number=position[i], line=i//3, column=i%3)
-            for i in range(len(position))]
+            for i in range(len(position))
+        ]
         return self.squares
 
-    def update(self, position):
-        for square in self.squares:
-            square.number = position[square.line][square.column]
+    def update(self, e):
+        """Passe à l'état suivant de la séquence et met à jour les tuiles."""
+        if self.current_step < len(sequence) - 1:
+            self.current_step += 1  # Aller à l’étape suivante
+            new_state = sequence[self.current_step]
+
+            for i, tile in enumerate(self.squares):
+                tile.value = str(new_state[i])  # Met à jour l'affichage
+
+            self.page.update()
+
 
 def main(page: ft.Page):
     page.title = TITLE
     page.window_height = HEIGHT
     page.window_width = WIDTH
 
-
     board = Board(page)
-
     grid = ft.GridView(expand=True, runs_count=3, spacing=5, run_spacing=5)
     grid.controls.extend(board.create_tiles(sequence[0]))  
-      
+
     buttons = ft.Row([
-        ft.IconButton(icon = ft.Icons.FIRE_TRUCK, on_click = board.update), 
-        ft.IconButton(icon = ft.Icons.LOCK),
-        ft.IconButton(icon = ft.Icons.FIRE_EXTINGUISHER), 
-        ft.IconButton(icon = ft.Icons.BIKE_SCOOTER)],
+        ft.IconButton(icon=ft.icons.FIRE_TRUCK, on_click=board.update), 
+        ft.IconButton(icon=ft.icons.LOCK),
+        ft.IconButton(icon=ft.icons.FIRE_EXTINGUISHER), 
+        ft.IconButton(icon=ft.icons.BIKE_SCOOTER)],
         alignment=ft.MainAxisAlignment.CENTER)
-    
+
     page.controls.append(grid)
     page.controls.append(buttons)
     page.update()
